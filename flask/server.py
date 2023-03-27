03af60f5e57d001f7d1f2c4331da83f9b7f05947
Alex McDonald <alexmcdonald712@gmail.com>
2023-03-26 21:37:21 -0600

    formatting

--- flask/server.py
+++ flask/server.py
@@ -1,17 +1,19 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api
 import json
 
 app = Flask(__name__)
 api = Api(app)
 
-class AllNames(Resource):
+#Endpoints
+class Names(Resource):
     def get(self):
         file = open('../WebScraping/babynames.json')
         names = json.load(file)
         return {'NAMES' : names}, 200
-
-api.add_resource(AllNames, '/')
+  
+#Paths
+api.add_resource(Names, '/')
 
 if __name__ == '__main__':
-    app.run() 
+    app.run(debug=True) 
