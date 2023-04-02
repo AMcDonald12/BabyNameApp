549c7d31b5c69c1a541c91849f2888ccda6454c3
Alex McDonald <alexmcdonald712@gmail.com>
2023-04-02 14:26:33 -0600

    created MAUI project, update gitignore

---
+++ BabyNameApp/BabyNameApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BabyNameApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ BabyNameApp/BabyNameApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BabyNameApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
