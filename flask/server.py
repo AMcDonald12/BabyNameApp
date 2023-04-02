9437df61851b6c16d9bc7b8ada9a7a45df222aad
Alex McDonald <alexmcdonald712@gmail.com>
2023-04-02 13:56:45 -0600

    created random name endpoint

--- flask/server.py
+++ flask/server.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import json
+import random
 
 app = Flask(__name__)
 api = Api(app)
 
 #Endpoints
-class Names(Resource):
+class AllNames(Resource):
     def get(self):
         file = open('../WebScraping/babynames.json')
         names = json.load(file)
-        return {'NAMES' : names}, 200
-  
+        return names, 200
+    
+class RandomName(Resource):
+    def get(self):
+        file = open('../WebScraping/babynames.json')
+        names = json.load(file)
+        gender = random.choice(list(names.keys()))
+        letter = random.choice(list(names[gender].keys()))
+        randomName = random.choice(names[gender][letter])
+        return {'Name':randomName,'Gender':gender}, 200
+      
 #Paths
-api.add_resource(Names, '/')
+api.add_resource(AllNames, '/')
+api.add_resource(RandomName, '/random')
 
 if __name__ == '__main__':
     app.run(debug=True) 
