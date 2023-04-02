279e68d9c9af29c7eeb64ddc22d4f956f317a94c
Alex McDonald <alexmcdonald712@gmail.com>
2023-04-02 15:26:18 -0600

    styled main page, init name service

---
+++ BabyNameApp/BabyNameApp/Services/NameService.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Json;
+
+namespace BabyNameApp.Services
+{
+	public class NameService
+	{
+        HttpClient httpClient;
+
+        public NameService()
+		{
+			httpClient = new HttpClient();
+		}
+	}
+}
+
